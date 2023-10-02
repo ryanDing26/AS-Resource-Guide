c346e613eca3d199cdfa6e0ade6cd676ded2cbac
ryanDing26 <110417507+ryanDing26@users.noreply.github.com>
2023-10-01 18:13:43 -0700

    Rough Draft of Website

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Filters the visible resource cards to those that are in a selected category
+function filterCategory(category) {
+
+}
+
+// Opens and closes the sidebar menu
+function toggleMenu() {
+
+}
